49817c15d66a654908226e1820c92aa826d6e208
Anibal Flores <floresanibal3.0@gmail.com>
2017-07-03 17:59:13 -0300

    Add files via upload

---
+++ Mascotas/Entidades/Loro.vb
@@ -0,0 +1,15 @@
+﻿Public Class Loro
+    'campos
+    Private _memoria As Queue(Of String)
+    Private _edad As Short
+    Private _fechaNacimiento As Date
+    Private _nombre As String
+
+    'constructor
+    Sub New()
+        _memoria = New Queue(Of String)
+        _edad = 0
+        _fechaNacimiento = Nothing
+        _nombre = ""
+    End Sub
+End Class
